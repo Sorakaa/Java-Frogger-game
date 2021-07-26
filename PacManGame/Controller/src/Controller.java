899cc9bfc51ad25fe7da23383d7aa009940a5ded
Ignaskaral <52499715+Ignaskaral@users.noreply.github.com>
2021-07-26 10:47:16 +0100

    Main game project

---
+++ PacManGame/Controller/src/Controller.java
@@ -0,0 +1,6 @@
+
+
+public class Controller implements IController {
+
+
+}
---
+++ PacManGame/Controller/src/Program.java
@@ -0,0 +1,12 @@
+/*
+* This class is the starting class of this game (Whole project)
+* Creation date: 2021/07/26
+* Created by: I. Karaliunas and V. Soraka
+* */
+
+public class Program {
+
+    //Declare Controller
+    public IController _controller = new Controller();
+
+}
